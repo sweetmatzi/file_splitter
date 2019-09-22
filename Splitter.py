1c891f10e636a634255053e687f0abfac9ee0002
sweetmatzi <matze.boehmann@gmail.com>
2019-09-22 20:14:09 +0200

    proggramm sammelt .png und .raw dateien und kopiert diese in respektive ornder

--- Splitter.py
+++ Splitter.py
@@ -2,12 +2,24 @@
 
 import sys
 import glob
+from shutil import copy
+
 
 def main():
+    rawdestination="/tmp/raw"
+    pngdestination="/tmp/png"
     for arg in sys.argv[0:]:
         print(arg)
-    for path in glob.iglob("/tmp/*.xlsx"):
+
+
+
+    for path in glob.iglob("/tmp/*.raw"):
         print(path)
+        copy(path, rawdestination)
+    for path in glob.iglob("/tmp/*.png"):
+        print(path)
+        copy(path, pngdestination)
+
 
 if __name__== "__main__":
   main()
