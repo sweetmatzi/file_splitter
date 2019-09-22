c64898c8dad9d3ada0b8006ed0b3c1548691ffae
sweetmatzi <55665428+sweetmatzi@users.noreply.github.com>
2019-09-22 21:21:21 +0200

    Add files via upload
    raw, jpg, mov in ordner kopieren

--- Splitter.py
+++ Splitter.py
@@ -6,19 +6,24 @@ from shutil import copy
 
 
 def main():
-    rawdestination="/tmp/raw"
-    pngdestination="/tmp/png"
+    jpgdestination="//Users/ich/Desktop/pyton/file_splitter-master/jpg"
+    cr2destination="//Users/ich/Desktop/pyton/file_splitter-master/cr2"
+    movdestination="//Users/ich/Desktop/pyton/file_splitter-master/mov"
+    source="/Volumes/EOS_DIGITAL/DCIM/100CANON"
     for arg in sys.argv[0:]:
         print(arg)
 
+    print(source)
 
-
-    for path in glob.iglob("/tmp/*.raw"):
+    for path in glob.iglob(source+"/*.jpg"):
         print(path)
-        copy(path, rawdestination)
-    for path in glob.iglob("/tmp/*.png"):
+        copy(path, cr2destination)
+    for path in glob.iglob(source+"/*.cr2"):
         print(path)
-        copy(path, pngdestination)
+        copy(path, cr2destination)
+    for path in glob.iglob(source+"/*.MOV"):
+        print(path)
+        copy(path, movdestination)
 
 
 if __name__== "__main__":
