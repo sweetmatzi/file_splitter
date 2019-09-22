8500096626b185e8f960167f026b2fead6d9bcf0
sweetmatzi <matze.boehmann@gmail.com>
2019-09-22 19:44:29 +0200

    file glob working

--- Splitter.py
+++ Splitter.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
 import sys
+import glob
 
 def main():
     for arg in sys.argv[0:]:
         print(arg)
+    for path in glob.iglob("/tmp/*.xlsx"):
+        print(path)
 
 if __name__== "__main__":
   main()
